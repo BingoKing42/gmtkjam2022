d45bb1ea79790ed160b0d8f87ed11bcfb1d86e17
ehammons11 <70654503+ehammons11@users.noreply.github.com>
2022-07-16 14:57:24 -0400

    Update DieSlot.cs

--- GMTK Jam Game 2022/Assets/Scripts/DieSlot.cs
+++ GMTK Jam Game 2022/Assets/Scripts/DieSlot.cs
@@ -12,7 +12,7 @@ public class DieSlot : MonoBehaviour, IDropHandler
    public void OnDrop(PointerEventData eventData)
    {
         Debug.Log("OnDrop");
-        if (eventData.pointerDrag != null)
+        if (eventData.pointerDrag != null && isEmpty)
         {
             eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
             slottedDie = eventData.pointerDrag.gameObject;
